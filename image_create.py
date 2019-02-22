c36b98afcc7ee6833064119ebccd0a2e921bf1d2
Kenny123Li <31445458+Kenny123Li@users.noreply.github.com>
2019-02-22 11:37:50 -0600

    commit so that I can pull

--- image_create.py
+++ image_create.py
@@ -1,7 +1,8 @@
 import cv2 as cv
 import numpy as np
 import os
-import sys
+import glob
+
 
 path = "negatives/"
 #path = "positives/"
@@ -12,12 +13,13 @@ folder = "neg/"
 dirs = os.listdir(path)
 num = 1
 
+CWD = os.getcwd()
+
 def gray_n_resize():
-    for images in dirs:
-        if os.path.isfile(path+images):
-            img = cv.imread(path+images+'.jpg', cv.IMREAD_GRAYSCALE)
-            res = cv.resize(img, (300,300))
-            cv.imwrite(folder+str(num)+'.jpg', res)
-            num += 1
+    for name in glob.glob(CWD+path+'/*'):
+        img = cv.imread(name, cv.IMREAD_GRAYSCALE)
+        res = cv.resize(img, (300, 300))
+        cv.imwrite(folder + str(num) + '.jpg', res)
+        num += 1
 
 gray_n_resize()
