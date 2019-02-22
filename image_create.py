ffb7320ca99c4632a41c24caf6ce007698d97a33
Kenny123Li <31445458+Kenny123Li@users.noreply.github.com>
2019-02-22 11:49:47 -0600

    this doesn't work don't have glob installed

--- image_create.py
+++ image_create.py
@@ -10,18 +10,18 @@ num = 1
 CWD = os.getcwd()
 
 def gray_n_resize():
-    for name in glob.glob(CWD+path+'/*'):
+    for name in glob.glob.glob(CWD + path + '/*'):
         img = cv.imread(name, cv.IMREAD_GRAYSCALE)
         res = cv.resize(img, (300, 300))
         cv.imwrite(folder + str(num) + '.jpg', res)
         num += 1
 
 def create_bg():
-	for filename in glob("negative/*"):
+	for filename in glob.glob("negative/*"):
 		line = filename+'\n'
 		with open('bg.txt','a') as f:
 			f.write(line)
 
 
-#gray_n_resize()
+gray_n_resize()
 #create_bg
